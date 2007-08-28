de5b337f44e1e4049673cfa5913374ad39d62adf
Dean Brettle <dean@brettle.com>
2007-08-28 07:20:56 +0000

    Fixed remaining compatibility bugs and did comprehensive testing.

--- neathtml.js
+++ neathtml.js
@@ -2,6 +2,8 @@
 NeatHtml - for preventing XSS in untrusted HTML
 Copyright (C) 2007  Dean Brettle
 
+GetParseErrorText() based on code from LGPLed sarissa.js which is Copyright 2004-2007 Emmanouil Batsis.
+
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
 License as published by the Free Software Foundation; either
@@ -39,23 +41,20 @@ NeatHtml.BeginUntrusted = function() {
 	}
 };
 
-NeatHtml.EndUntrusted = function() {
+NeatHtml.ProcessUntrusted = function() {
 	var my = this;
 	var containingDiv = FindContainingDiv(this.BeginUntrustedScript);
 	try
 	{
 		var xmlStr = GetUntrustedXml();
 		xmlStr = ProcessXml(xmlStr);
-		
 	   containingDiv.innerHTML = xmlStr;
-	
 	   ProcessHtmlElem(containingDiv);
 	}
 	catch (ex)
 	{
 		containingDiv.innerHTML = "<pre>" + ex.toString().replace("<", "&lt;").replace("&", "&amp;") + "</pre>";
 	}
-	
 	/***** Local Functions ******/
 	
 	function FindContainingDiv(n) 
@@ -113,34 +112,70 @@ NeatHtml.EndUntrusted = function() {
 		var xmlDoc;
 		if (window.DOMParser)
 		{
+			// Mozilla...
 			xmlDoc = (new DOMParser()).parseFromString(xmlStr, "text/xml");
 		}
 		else
 		{
 			try
 			{
+				// Safari/Konqueror...
 				xmlDoc = document.implementation.createDocument("", null, null);
 			}
 			catch (ex) 
 			{
+				// IE...
 				xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
 			}
 		
 			xmlDoc.async = false;
 			xmlDoc.loadXML(xmlStr);
 		}
-		if ((xmlDoc.parseError && xmlDoc.parseError.errorCode)
-			|| xmlDoc.documentElement.tagName == "parsererror"
-			|| xmlDoc.documentElement.tagName == "html" // Konqueror returns an error HTML document
-			|| xmlDoc.getElementsByTagName("parsererror").length > 0)
-		{
-/*
-			var parseErrorText = xmlDoc.documentElement.firstChild.data;
-         parseErrorText += "\n" +  xmlDoc.documentElement.firstChild.nextSibling.firstChild.data;
-*/
-	  		throw "Untrusted HTML could not be parsed";
+		var parseErrorText = GetParseErrorText(xmlDoc);
+      if (parseErrorText)
+      {
+	  		throw "Untrusted HTML could not be parsed: " + parseErrorText;
 		}
 		return xmlDoc;
+	}
+	
+	function GetParseErrorText(xmlDoc)
+	{
+		var parseErrorText = null;
+		if (xmlDoc.parseError && xmlDoc.parseError.errorCode)
+		{
+			parseErrorText = xmlDoc.parseError.reason + 
+				"\nLocation: " + xmlDoc.parseError.url + 
+				"\nLine Number " + xmlDoc.parseError.line + ", Column " + 
+			xmlDoc.parseError.linepos + 
+				":\n" + xmlDoc.parseError.srcText +
+				"\n";
+			for(var i = 0;  i < xmlDoc.parseError.linepos;i++)
+			{
+				parseErrorText += "-";
+			};
+			parseErrorText +=  "^\n";
+		}
+		else if (!xmlDoc.documentElement)
+		{
+			parseErrorText = "Unknown reason";
+		}
+		else if (xmlDoc.documentElement.tagName == "parsererror"
+					|| xmlDoc.documentElement.tagName == "html" // Konqueror returns an error HTML document
+					|| xmlDoc.getElementsByTagName("parsererror").length > 0)
+		{
+			parseErrorText = "Unknown reason";
+			if (xmlDoc.documentElement.firstChild && xmlDoc.documentElement.firstChild.data)
+			{ 
+				parseErrorText = xmlDoc.documentElement.firstChild.data;
+				if (xmlDoc.documentElement.firstChild.nextSibling
+					&& xmlDoc.documentElement.firstChild.nextSibling.firstChild)
+				{
+      			parseErrorText += "\n" +  xmlDoc.documentElement.firstChild.nextSibling.firstChild.data;
+      		}
+      	}
+      }
+      return parseErrorText;
 	}
 	
 	function XmlDocToString(xmlDoc)
@@ -152,21 +187,70 @@ NeatHtml.EndUntrusted = function() {
 			if (!xmlStr)
 			{
 				// Safari < 2.0 only accepts XML documents, not nodes.
-				xmlStr = new XMLSerializer().serializeToString(xmlDoc);
+				// It also does not correctly serialize attributes that have been accessed with elem.attributes.item(i).
+				// So, we serialize manually.
+				xmlStr = XmlElemToString(xmlDoc.firstChild);
   			}
   		} 
   		else
   		{
-  			xmlStr = xmlDoc.firstChild.xml;
+  			xmlStr = xmlDoc.documentElement.xml;
   		}
 		return xmlStr;
 	}
 	
+	function XmlElemToString(elem)
+	{
+		var xmlStr = "";
+		
+		xmlStr = "<" + elem.tagName;
+		var attrs = elem.attributes;
+		for (var i = 0; i < attrs.length; i++)
+		{
+			var attr = attrs.item(i);
+			xmlStr += " " + attr.name + '="' + HtmlEncode(attr.value) + '"';
+		}
+		xmlStr += ">";
+		var nextSibling = null;
+		for (var n = elem.firstChild; n != null; n = nextSibling)
+		{
+			nextSibling = n.nextSibling; // Remember the nextSibling in case n gets removed.
+			switch (n.nodeType)
+			{
+				case 1: // ELEMENT_NODE
+					xmlStr += XmlElemToString(n);
+					break;
+				case 3: // TEXT_NODE
+					xmlStr += HtmlEncode(n.nodeValue);
+					break;
+				default:
+					// There shouldn't be anything else.  We removed comments and CDATA sections.
+					// Character entity references should have been expanded by the parser.
+			}
+		}
+		xmlStr += "</" + elem.tagName + ">";
+		return xmlStr;
+	}
+	
+	function HtmlEncode(s)
+	{
+		return s.replace(/[<>&"']/g, function (c) { 
+			switch (c)
+			{
+				case '<': return "&lt;";  
+				case '>': return "&gt;";  
+				case '&': return "&amp;";  
+				case '"': return "&quot;";  
+				case "'": return "&apos;";
+			}
+		});  
+	}
+	
 	function ProcessXmlElem(elem)
 	{
-		if (elem.tagName in { SCRIPT: 1, OBJECT: 1, EMBED: 1, IFRAME: 1, FRAME: 1, FRAMESET: 1, XML: 1 })
+		if (elem.tagName.toLowerCase() in { script: 1, object: 1, embed: 1, iframe: 1, frame: 1, frameset: 1, xml: 1 })
 		{
-			elem.parentNode.removeNode(elem);
+			elem.parentNode.removeChild(elem);
 			return;
 		}
 		if (elem.attributes.length > 0)
@@ -185,8 +269,10 @@ NeatHtml.EndUntrusted = function() {
 				case 3: // TEXT_NODE
 				case 5: // ENTITY_REFERENCE_NODE
 					break;
+				case 4: // CDATA_SECTION_NODE
+				case 8: // COMMENT_NODE
 				default: // Remove everything else, including comments and CDATA sections
-					elem.removeNode(n);
+					elem.removeChild(n);
 			}
 		}
 	}
@@ -215,16 +301,48 @@ NeatHtml.EndUntrusted = function() {
 				attrsToRemove.push(attr);
 			}
 		}
+
 		// Remove the unwanted attributes
 		for (var i = 0; i < attrsToRemove.length; i++)
 		{
 			elem.removeAttributeNode(attrsToRemove[i]);
 		}
-		// If there was a style attribute, put it's value in our private attribute.
+		// If there was a style attribute, sanitize it's value and put it in our private attribute.
+		// We must sanitize the style before we attempt to access any style properties (e.g. elem.style.overflow) in IE.
+		// There doesn't seem to be a way to access the styles in IE after they have been parsed but before the 
+		// script has been run.  For example, if you do:
+		//
+		// elem.style.cssText = "attr: expression(window.alert('XSS'));";
+		//
+		// the script will not run yet.  But, as soon as you access any of the style properties the script will run. 
 		if (styleValue != null)
 		{
+			styleValue = SanitizeStyle(styleValue);
 			elem.setAttribute("neathtml_style", styleValue);
 		}
+	}
+	
+	// Removes Javascript from style.
+	function SanitizeStyle(s)
+	{
+		// Remove comments
+		s = s.replace(/\x2F\*[^*]*\*+([^\x2F][^*]*\*+)*\x2F/gm, "");
+		// Remove any left over comment markers
+		s = s.replace(/\x2F\*|\*\x2F/, "");
+		// Remove Unicode escapes
+		s = s.replace(/\\[0-9a-f]{1,6}[ \n\r\t\f]?/g, "");
+		// Remove all other escapes
+		s = s.replace(/\\./g, "");
+		
+		// Rename all function calls except those to rgb()
+		s = s.replace(/([A-Za-z]|[^\0-\177])([A-Za-z0-9-]|[^\0-\177])*\(/g, function (match) {
+			if (match != "rgb(")
+			{
+				match = match.substring(0, match.length-1) + "NotAllowedByNeatHtml(";
+			}
+			return match;
+		});
+		return s;
 	}
 
 	function ProcessHtmlElem(parent)
@@ -285,7 +403,7 @@ NeatHtml.EndUntrusted = function() {
 				 	testElem = document.createElement("A");
 				}
 				testElem.style.cssText = val;
-				CopySafeStyleProperties(testElem, elem);
+				CopyStyleProperties(testElem, elem);
 			}
 		}
 		// Remove the unwanted attributes
@@ -295,22 +413,64 @@ NeatHtml.EndUntrusted = function() {
 		}
 	}
 	
-	function CopySafeStyleProperties(src, dest)
+	function CopyStyleProperties(src, dest)
 	{
-		// Copy the style properties that don't contain script
-		for (var p in src.style)
+		// TODO: Filter properties when copying them.
+
+		if (src.style.length == 0)
 		{
-			var propValue = src.style[p];
-			var propType = typeof(propValue);
-			if (p == "cssText" || p == "length"          
-				|| (propType != "string" && propType != "number" && propType != "boolean") 
-				|| propValue == null || propValue == ""
-				|| propValue.match(/expression|url/i)) 
+			return;
+		}
+		// Copy the style properties
+		if (src.style.getPropertyValue && dest.style.setProperty)
+		{
+			var propNames = [];
+			if (src.style.item(0) !== null)
 			{
-				continue;
+				// The standard way to get the property names...
+				for (var i = 0; i < src.style.length; i++)
+				{
+					propNames.push(src.style.item(i));
+				}
 			}
-			dest.style[p] = propValue;
- 		}			
+			else
+			{
+				// The Safari hack...
+				var styles = src.style.cssText.split(/;/g);
+				for (var i = 0; i < styles.length; i++)
+				{
+					var name = styles[i].replace(/[ \t\n\r]/, "").split(/:/g)[0];
+					propNames.push(name);
+				}
+			}
+			for (var i = 0; i < propNames.length; i++)
+			{
+				var p = propNames[i];
+				var propValue = src.style.getPropertyValue(p);
+				if (propValue != "")
+				{
+					dest.style.setProperty(p, propValue, null);
+				}
+			}
+		}
+		else
+		{
+			// The IE way
+			for (var p in src.style)
+			{
+				if (p == "cssText" || p == "length") continue;
+				var propValue = null;
+				var propType = null;
+				propValue = src.style[p];
+				propType = typeof(propValue);
+				if((propType != "string" && propType != "number" && propType != "boolean") 
+					|| propValue == null || propValue == "")
+				{
+					continue;
+				}
+				dest.style[p] = propValue;
+	 		}
+	 	}
 	}
 };
 
