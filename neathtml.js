2624c1e41119e2a21f20babd1036b1064bd00ffe
Dean Brettle <dean@brettle.com>
2007-08-30 01:21:30 +0000

    Added handling of entity refs and unencoded ampersands.

--- neathtml.js
+++ neathtml.js
@@ -146,7 +146,7 @@ NeatHtml.Filter.prototype.AllowAttribute = function(elem, attr, attrsToRemove, a
 NeatHtml.Filter.prototype.HandleUrl = function(elem, attr, attrsToRemove, attrsToAdd)
 {
 	if (attr.value == null) return;
-	if (! /^(http:|https:|ftp:|mailto:)/i.test(attr.value) == false)   // ... invalid protocol
+	if (/^(http:|https:|ftp:|mailto:)/i.test(attr.value) == false)   // ... invalid protocol
 	{
 		attrsToRemove.push(attr);
 	}
@@ -340,15 +340,58 @@ NeatHtml.Filter.prototype.ProcessUntrusted = function() {
 		// According to HTML 3.2
 		var endTagsForbidden = { br:1, hr:1, meta:1, col:1, isindex:1, img:1, link:1, area:1, basefont:1, param:1, input:1, base:1 };
 		var endTagsOptional = { li:1, p:1, dt:1, dd:1, thead:1, tfoot:1, tbody:1, colgroup:1, tr:1, th:1, td:1, plaintext:1, option:1 };
+		var entityCharCodes = { 
+			nbsp:160,iexcl:161,cent:162,pound:163,curren:164,yen:165,brvbar:166,
+			sect:167,uml:168,copy:169,ordf:170,laquo:171,not:172,shy:173,reg:174,
+			macr:175,deg:176,plusmn:177,sup2:178,sup3:179,acute:180,micro:181,
+			para:182,middot:183,cedil:184,sup1:185,ordm:186,raquo:187,frac14:188,
+			frac12:189,frac34:190,iquest:191,Agrave:192,Aacute:193,Acirc:194,
+			Atilde:195,Auml:196,Aring:197,AElig:198,Ccedil:199,Egrave:200,Eacute:201,
+			Ecirc:202,Euml:203,Igrave:204,Iacute:205,Icirc:206,Iuml:207,ETH:208,
+			Ntilde:209,Ograve:210,Oacute:211,Ocirc:212,Otilde:213,Ouml:214,times:215,
+			Oslash:216,Ugrave:217,Uacute:218,Ucirc:219,Uuml:220,Yacute:221,
+			THORN:222,szlig:223,agrave:224,aacute:225,acirc:226,atilde:227,
+			auml:228,aring:229,aelig:230,ccedil:231,egrave:232,eacute:233,
+			ecirc:234,euml:235,igrave:236,iacute:237,icirc:238,iuml:239,eth:240,
+			ntilde:241,ograve:242,oacute:243,ocirc:244,otilde:245,ouml:246,
+			divide:247,oslash:248,ugrave:249,uacute:250,ucirc:251,uuml:252,yacute:253,
+			thorn:254,yuml:255,fnof:402,Alpha:913,Beta:914,Gamma:915,Delta:916,
+			Epsilon:917,Zeta:918,Eta:919,Theta:920,Iota:921,Kappa:922,Lambda:923,
+			Mu:924,Nu:925,Xi:926,Omicron:927,Pi:928,Rho:929,Sigma:931,Tau:932,
+			Upsilon:933,Phi:934,Chi:935,Psi:936,Omega:937,alpha:945,beta:946,
+			gamma:947,delta:948,epsilon:949,zeta:950,eta:951,theta:952,iota:953,
+			kappa:954,lambda:955,mu:956,nu:957,xi:958,omicron:959,pi:960,rho:961,
+			sigmaf:962,sigma:963,tau:964,upsilon:965,phi:966,chi:967,psi:968,
+			omega:969,thetasym:977,upsih:978,piv:982,bull:8226,hellip:8230,prime:8242,
+			Prime:8243,oline:8254,frasl:8260,weierp:8472,image:8465,real:8476,
+			trade:8482,alefsym:8501,larr:8592,uarr:8593,rarr:8594,darr:8595,harr:8596,
+			crarr:8629,lArr:8656,uArr:8657,rArr:8658,dArr:8659,hArr:8660,
+			forall:8704,part:8706,exist:8707,empty:8709,nabla:8711,isin:8712,
+			notin:8713,ni:8715,prod:8719,sum:8721,minus:8722,lowast:8727,radic:8730,
+			prop:8733,infin:8734,ang:8736,and:8743,or:8744,cap:8745,cup:8746,int:8747,
+			there4:8756,sim:8764,cong:8773,asymp:8776,ne:8800,equiv:8801,le:8804,
+			ge:8805,sub:8834,sup:8835,nsub:8836,sube:8838,supe:8839,oplus:8853,otimes:8855,
+			perp:8869,sdot:8901,lceil:8968,rceil:8969,lfloor:8970,rfloor:8971,
+			lang:9001,rang:9002,loz:9674,spades:9824,clubs:9827,hearts:9829,diams:9830,
+			OElig:338,oelig:339,Scaron:352,scaron:353,Yuml:376,circ:710,tilde:732,
+			ensp:8194,emsp:8195,thinsp:8201,zwnj:8204,zwj:8205,lrm:8206,rlm:8207,
+			ndash:8211,mdash:8212,lsquo:8216,rsquo:8217,sbquo:8218,ldquo:8220,rdquo:8221,
+			bdquo:8222,dagger:8224,Dagger:8225,permil:8240,lsaquo:8249,rsaquo:8250,euro:8364 
+		};
 		var openTagNames = [];
 		var lengthToIgnoreAtEnd = 0;
 		var lengthToIgnoreAtBeginning = 0;
 		var ignoreUntilOffset = 0;
-		s = s.replace(/<(\/?)(([!\?A-Z:_a-z][^ \t\n\r>]*)([^>]*)>|([^!\?A-Z:_a-z]))/gm, function(match, isEndTag, raw, tagName, attrs, isNotEncoded, offset) {
+		s = s.replace(/(&((#[0-9]{1,10};|#x[0-9a-fA-F]{1,8};|amp;|lt;|gt;|quot;|apos;)|([A-Z:_a-z][A-Z:_a-z0-9._]{0,10};|(#X[0-9a-fA-F]{1,8};|))))|(<(\/?)(([!\?A-Z:_a-z][^ \t\n\r>]*)([^>]*)>|([^!\?A-Z:_a-z])))/gm, 
+			function(match, isAmp, afterAmp, isValidXmlEntityRef, charEntityRef, upperCaseHexEntityRef, isOpenAngle, isEndTag, raw, tagName, attrs, isNotEncoded, offset) {
 			// If we already set the length to ignore at end, then we are already done.
 			if (lengthToIgnoreAtEnd || offset < ignoreUntilOffset)
 			{ 
 				return match;
+			}
+			if (isAmp)
+			{
+				return HandleAmpersand(match, isValidXmlEntityRef, afterAmp, charEntityRef, upperCaseHexEntityRef);
 			}
 			if (isNotEncoded)
 			{
@@ -427,6 +470,8 @@ NeatHtml.Filter.prototype.ProcessUntrusted = function() {
 					attrValue = attrName;
 				}
 				attrValue = attrValue.replace(/</gm, "&lt;");
+				attrValue = attrValue.replace(/&((#[0-9]{1,10};|#x[0-9a-fA-F]{1,8};|amp;|lt;|gt;|quot;|apos;)|([A-Z:_a-z][A-Z:_a-z0-9._]{0,10};|(#X[0-9a-fA-F]{1,8};|)))/gm,
+														HandleAmpersand);
 				var firstChar = attrValue.charAt(0)
 				
 				if ( firstChar != '"' && firstChar != "'")
@@ -473,8 +518,29 @@ NeatHtml.Filter.prototype.ProcessUntrusted = function() {
 		 	var openTagName = openTagNames.pop();
 			s += "</" + openTagName + ">";
 		}
-
+// alert(s);
 		return s;
+
+		/* Local functions */
+		function HandleAmpersand(match, isValidXmlEntityRef, afterAmp, charEntityRef, upperCaseHexEntityRef)
+		{
+			if (isValidXmlEntityRef)
+				return match;
+			if (!charEntityRef)
+				return HtmlEncode(match);
+			if (upperCaseHexEntityRef)
+				return match.toLowerCase();
+			// It is a character entity reference but it isn't supported in XML
+			if (charEntityRef)
+			{
+				// Strip the trailing semicolon
+				charEntityRef = charEntityRef.substring(0, charEntityRef.length-1);
+			}
+			var charCode = entityCharCodes[charEntityRef];
+			if (!charCode)
+				return HtmlEncode(match);
+			return "&#" + charCode + ";";
+		}
 	}
 
 	function ProcessXml(xmlStr)
